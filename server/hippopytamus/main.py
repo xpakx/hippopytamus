0a15e0c33d4585c833a058a9bd2757d979dcc854
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-12 19:57:16 +0200

    fix: reuse socket address

--- server/hippopytamus/main.py
+++ server/hippopytamus/main.py
@@ -8,6 +8,7 @@ port = 8000
 class TCPServer:
     def listen(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # TODO: timeout (maybe?)
         sock.bind((host, port))
 
