eea757c4347fe678ee41386b7d1165cb2561cee3
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-12 19:56:15 +0200

    feat: add basic single thread blocking server

---
+++ server/hippopytamus/main.py
@@ -0,0 +1,28 @@
+import socket
+
+
+host = '127.0.0.1'
+port = 8000
+
+
+class TCPServer:
+    def listen(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # TODO: timeout (maybe?)
+        sock.bind((host, port))
+
+        sock.listen()
+        print(sock.getsockname())
+
+        while True:
+            connection, address = sock.accept()
+
+            print(f"new client: {address}")
+            data = connection.recv(1024)  # TODO: protocol should decide
+            connection.sendall(data)
+            connection.close()
+
+
+if __name__ == "__main__":
+    server = TCPServer()
+    server.listen()
