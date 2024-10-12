f52ef418151a85b206ec3a6ca303f40c200d0883
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-12 20:07:57 +0200

    feat: implement HTTP/0.9

--- server/hippopytamus/main.py
+++ server/hippopytamus/main.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 
 host = '127.0.0.1'
@@ -33,7 +34,43 @@ class EchoProtocol:
         return request
 
 
+class HttpProtocol09:
+    def process_request(self, request: bytes):
+        header, uri = self.parse_request(request)
+        print(f"Method: {str(header)}")
+        print(f"Resource: {str(uri)}")
+        # TODO: should only parse request,
+        # move constructing response to service
+        if header != b"GET":
+            return b"<h1>Unsupported!</h1>"
+        if uri != b"/":
+            return self.not_found()
+        body, err = self.body_from_file("index.html")
+        if err:
+            return self.not_found()
+        return body
+
+    def parse_request(self, request: bytes) -> (str, str):
+        lines = request.split(b"\r\n")
+        header = lines[0].split(b" ")
+        if len(header) < 2:
+            return None
+        method = header[0]
+        uri = header[1]
+        return method, uri
+
+    def not_found(self) -> bytes:
+        return b"<html><body><h1>No such file</h1></body></html>"
+
+    def body_from_file(self, url: str) -> (bytes, str):
+        if os.path.exists(url):
+            with open(url, 'rb') as f:
+                body = f.read()
+            return body, None
+        return None, "No such file"
+
+
 if __name__ == "__main__":
-    protocol = EchoProtocol()
+    protocol = HttpProtocol09()
     server = TCPServer(protocol)
     server.listen()
