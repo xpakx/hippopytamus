7aeae1cf2663115b500e5492cbf25d5770a33a1f
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-12 20:33:24 +0200

    feat: implement basic static page for http/1.0

--- server/hippopytamus/main.py
+++ server/hippopytamus/main.py
@@ -70,7 +70,62 @@ class HttpProtocol09:
         return None, "No such file"
 
 
+class HttpProtocol10:
+    codes = {200: b"OK", 501: b"Not Implemented", 404: b"Not Found"}
+
+    def process_request(self, request: bytes):
+        req = self.parse_request(request)
+        print(f"Method: {req['method']}")
+        print(f"Resource: {req['uri']}")
+        # TODO: move to service
+        if req['method'] != "GET":
+            return self.response(code=501)
+        if req['uri'] != "/":
+            body, _ = self.body_from_file("404.html")
+            return self.response(code=404, body=body)
+        body, err = self.body_from_file("index.html")
+        if err:
+            body, _ = self.body_from_file("404.html")
+            return self.response(code=404, body=body)
+        return self.response(body=body)
+
+    def response(self, code: int = 200, body: str = None):
+        response = b"HTTP/1.0 "
+        response += bytes(str(code), "ascii")
+        response += b" "
+        response += self.codes[code]
+        response += b"\r\n"
+        response += b"Server: Hippopytamus\r\n"
+        if body:
+            response += b"Content-Type: text/html\r\n"
+            response += b"\r\n"
+            response += body
+        return response
+
+    def parse_request(self, request: bytes):
+        lines = request.split(b"\r\n")
+        header = lines[0].split(b" ")
+        if len(header) != 3:
+            return None
+        method = header[0].decode('utf-8')
+        uri = header[1].decode('utf-8')
+        version = header[2].decode('utf-8')
+        request = {
+                "method": method,
+                "uri": uri,
+                "version": version
+        }
+        return request
+
+    def body_from_file(self, url: str) -> (bytes, str):
+        if os.path.exists(url):
+            with open(url, 'rb') as f:
+                body = f.read()
+            return body, None
+        return None, "No such file"
+
+
 if __name__ == "__main__":
-    protocol = HttpProtocol09()
+    protocol = HttpProtocol10()
     server = TCPServer(protocol)
     server.listen()
