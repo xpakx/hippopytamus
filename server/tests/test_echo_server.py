ddb3cd69caf04be73b486dd3d58bec2be50682cd
xpakx <55586470+xpakx@users.noreply.github.com>
2024-10-31 17:35:59 +0100

    test: make sure socket for tests can be reused

--- server/tests/test_echo_server.py
+++ server/tests/test_echo_server.py
@@ -8,6 +8,7 @@ from typing import cast, Generator
 
 def get_free_port() -> int:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("", 0))
         return cast(int, s.getsockname()[1])
 
