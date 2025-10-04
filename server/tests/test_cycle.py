6b1d805c280e3f29bbc2fbc11adee0a6a1d81e19
xpakx <55586470+xpakx@users.noreply.github.com>
2025-10-04 20:44:14 +0200

    test: test exception handler

---
+++ server/tests/test_cycle.py
@@ -0,0 +1,30 @@
+import pytest
+import threading
+from hippopytamus.core.app import HippoApp, ServerOptions
+from .utils import TestClient, get_free_port
+
+
+@pytest.fixture
+def app_server() -> int:
+    port = get_free_port()
+    app = HippoApp("hippopytamus.example.example2", ServerOptions(port=port, host="localhost"))
+    thread = threading.Thread(target=app.run, daemon=True)
+    thread.start()
+    return port
+
+
+@pytest.fixture
+def client(app_server: int) -> TestClient:
+    c = TestClient()
+    c.connect("localhost", app_server)
+    try:
+        yield c
+    finally:
+        c.close()
+
+
+def test_hello_triggers_exception_handler(client: TestClient):
+    resp = client.get("/hello")
+
+    assert "500" in resp.status
+    assert "Test error" in resp.body
