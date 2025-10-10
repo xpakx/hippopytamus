6353449958b6e7c8bf4fd5ea47f01817785c8b1b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-10-10 16:56:18 +0200

    chore: update example for exception handlers

--- server/hippopytamus/example/example3/components.py
+++ server/hippopytamus/example/example3/components.py
@@ -8,7 +8,16 @@ class MyService:
     def exception_request(self) -> str:
         raise NotFoundException()
 
+    @GetMapping("/exception2")
+    def exception2_request(self) -> str:
+        raise NotFoundWithReasonException()
+
 
 @ResponseStatus(code=404)
 class NotFoundException(Exception):
+    pass
+
+
+@ResponseStatus(code=404, reason="Test Not Found")
+class NotFoundWithReasonException(Exception):
     pass
--- server/hippopytamus/example/example3/main.py
+++ server/hippopytamus/example/example3/main.py
@@ -1,5 +1,12 @@
 from hippopytamus.core.app import HippoApp
+from hippopytamus.core.container import HippoContainer
+from hippopytamus.core.exception import HippoExceptionManager
+from hippopytamus.logger.logger import LoggerFactory
 
 if __name__ == "__main__":
-    app = HippoApp("hippopytamus.example.example1")
+    factory = LoggerFactory.get_factory()
+    factory.disable_all()
+    factory.enable_for(HippoContainer)
+    factory.enable_for(HippoExceptionManager)
+    app = HippoApp("hippopytamus.example.example3")
     app.run()
